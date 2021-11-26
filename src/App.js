eb03262cd93e9fbfc83ac16d18b409db5b36562a
MarkoDev03 <mmarko.perovici3@gmail.com>
2021-11-27 00:20:45 +0100

    -first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const App = () => {
+
+   const [newArticles, setNewsArticles] = useState([])
 
    const alanAIkey = "0cec60ce0293f675fe5785b0dc7bb4b82e956eca572e1d8b807a3e2338fdd0dc/stage";
 
    useLayoutEffect(() => {
        alanBtn({
            key:alanAIkey,
-           onCommand: ({ command }) => {
-               if (command === "testCommand") {
-                   alert("Test command is executed")
+           onCommand: ({ command, country }) => {
+               if (command === "newHeadlines") {
+                   setNewsArticles(country)
                }
            }
        })
@@ -18,7 +21,7 @@ const App = () => {
 
     return (
         <div>
-            ALAN AI
+            <NewsCards articles={newArticles} />
         </div>
     )
 }
---
+++ src/components/NewsCard/NewsCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsCard = ({article}) => {
+    return (
+        <div>
+            <h1>{article.title}</h1>
+        </div>
+    )
+}
+
+export default NewsCard
